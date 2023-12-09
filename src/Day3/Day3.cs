a728ac59fad17417dded5d43a01ee5dd88afb8f0
Stephen Shaw <sshaw@decriptor.com>
2023-12-09 09:36:43 -0700

    Add some Day3 stuff

--- src/Day3/Day3.cs
+++ src/Day3/Day3.cs
@@ -7,6 +7,33 @@ static class Day3
     public static void Main(string[] args)
     {
         var data = GetInputs(3);
-        
+
+        ParseData(data);
+    }
+
+    private static void ParseData(IReadOnlyList<string> data)
+    {
+        var adjacents = new Dictionary<string, int>();
+        for (int i = 0; i < data.Count; i++)
+        {
+            for (int j = 0; j < data[i].Length; j++)
+            {
+                var target = data[i][j];
+                if (target != '.')
+                {
+                    var number = target.ToString();
+                    if (char.IsNumber(target))
+                    {
+                        while (j < data[i].Length && char.IsNumber(data[i][j + 1]))
+                        {
+                            target += data[i][++j];
+                        }
+                    }
+
+                    Console.WriteLine($"Found: {target}. Index [{i}][{j}]");
+                    // target [i][j]
+                }
+            }
+        }
     }
 }
--- src/Utils/Utils.cs
+++ src/Utils/Utils.cs
@@ -4,7 +4,7 @@ public static class Utils
 {
     public static string[] GetInputs(int day)
     {
-        var path = Path.Combine("d:", "code", "AdventOfcode", "Inputs", $"Day{day}Input.txt");
+        var path = Path.Combine("..", "..", "..", "..", "..", "Inputs", $"Day{day}Input.txt");
         if (!Path.Exists(path))
             throw new FileNotFoundException(path);
         
